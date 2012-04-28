515c9775be04e76e4e34d3a70ba6e689aac86cb5
yujiorama <yujiorama+github@gmail.com>
2012-04-28 17:02:59 +0900

    refactoring some field, checking call styles

--- goos-auction/src/test/java/com/github/yujiorama/auctionsinper/AuctionMessageTranslatorTest.java
+++ goos-auction/src/test/java/com/github/yujiorama/auctionsinper/AuctionMessageTranslatorTest.java
@@ -4,17 +4,21 @@ package com.github.yujiorama.auctionsinper;
 import org.jivesoftware.smack.packet.Message;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JMock.class)
 public class AuctionMessageTranslatorTest {
+
+	private final Mockery context = new Mockery();
+	private final AuctionEventListener listener = context.mock(AuctionEventListener.class);
 
 	@Test
 	public void notify_auction_closed_when_close_message_received() {
-		Mockery context = new Mockery();
-		Expectations expectations = new Expectations();
-		AuctionEventListener listener = context.mock(AuctionEventListener.class);
-		expectations.oneOf(listener).auctionClosed();
-		context.checking(expectations);
+		context.checking(new Expectations(){{
+			oneOf(listener).auctionClosed();
+		}});
 		
 		Message message = new Message();
 		message.setBody("SOLVersion: 1.1; Event: CLOSE;");
@@ -24,11 +28,9 @@ public class AuctionMessageTranslatorTest {
 
 	@Test
 	public void notify_bid_details_when_current_price_message_received() {
-		Mockery context = new Mockery();
-		Expectations expectations = new Expectations();
-		AuctionEventListener listener = context.mock(AuctionEventListener.class);
-		expectations.exactly(1).of(listener).currentPrice(192, 7);
-		context.checking(expectations);
+		context.checking(new Expectations(){{
+			exactly(1).of(listener).currentPrice(192, 7);
+		}});
 		
 		Message message = new Message();
 		message.setBody("SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;");
