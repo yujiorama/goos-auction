2429c1e9dbd26a293a68a6d457260fb6cfb712b8
yujiorama <yujiorama+github@gmail.com>
2012-05-01 20:34:11 +0900

    Replacing JLabel

--- goos-auction/src/main/java/com/github/yujiorama/auctionsinper/MainWindow.java
+++ goos-auction/src/main/java/com/github/yujiorama/auctionsinper/MainWindow.java
@@ -1,38 +1,70 @@
 package com.github.yujiorama.auctionsinper;
 
-import java.awt.Color;
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Rectangle;
 
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.border.LineBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
 
 public class MainWindow extends JFrame {
 
+	public static class SniperTableModel extends AbstractTableModel {
+		private static final long serialVersionUID = 1L;
+		private String statusText = AuctionStatus.JOINING.toString();
+		
+		@Override
+		public int getRowCount() {
+			return 1;
+		}
+
+		@Override
+		public int getColumnCount() {
+			return 1;
+		}
+
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			return statusText;
+		}
+
+		public void setStatusText(String newStatus) {
+			this.statusText = newStatus;
+			fireTableCellUpdated(0, 0);
+		}
+	}
+
 	private static final long serialVersionUID = 1L;
+	private static final String APPLICATION_NAME = "Auction Sniper";
 	public static final String MAIN_WINDOW_NAME = "auction-sniper";
 	public static final String SNIPER_STATUS_NAME = "sniper status";
-	private JLabel sniperStatus = newLabel(AuctionStatus.JOINING);
+	public static final String SNIPER_TABLE_NAME = "sniper table";
+	private SniperTableModel sniperTableModel = new SniperTableModel();
+	private JTable snipersTable = newTable(SNIPER_TABLE_NAME);
 
 	public MainWindow() {
-		super("Auction Sniper");
+		super(APPLICATION_NAME);
 		setName(MAIN_WINDOW_NAME);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(new Rectangle(new Point(50, 50), new Dimension(320, 240)));
-		add(sniperStatus);
+		
+		Container container = getContentPane();
+		container.setLayout(new BorderLayout());
+		container.add(new JScrollPane(snipersTable), BorderLayout.CENTER);
 		setVisible(true);
 	}
 
-	private JLabel newLabel(AuctionStatus auctionStatus) {
-		JLabel label = new JLabel(auctionStatus.toString());
-		label.setName(SNIPER_STATUS_NAME);
-		label.setBorder(new LineBorder(Color.BLACK));
-		return label;
+	private JTable newTable(String name) {
+		JTable table = new JTable(sniperTableModel);
+		table.setName(name);
+		return table;
 	}
 
 	public void showStatus(AuctionStatus auctionStatus) {
-		sniperStatus.setText(auctionStatus.toString());
+		sniperTableModel.setStatusText(auctionStatus.toString());
 	}
 }
--- goos-auction/src/test/java/com/github/yujiorama/auctionsinper/AuctionSniperDriver.java
+++ goos-auction/src/test/java/com/github/yujiorama/auctionsinper/AuctionSniperDriver.java
@@ -1,10 +1,11 @@
 package com.github.yujiorama.auctionsinper;
 
+import static com.objogate.wl.swing.matcher.JLabelTextMatcher.withLabelText;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 import com.objogate.wl.swing.AWTEventQueueProber;
 import com.objogate.wl.swing.driver.JFrameDriver;
-import com.objogate.wl.swing.driver.JLabelDriver;
+import com.objogate.wl.swing.driver.JTableDriver;
 import com.objogate.wl.swing.gesture.GesturePerformer;
 
 @SuppressWarnings("unchecked")
@@ -17,6 +18,6 @@ public class AuctionSniperDriver extends JFrameDriver {
 	}
 
 	public void showSniperStatus(AuctionStatus auctionStatus) {
-		new JLabelDriver(this, named(MainWindow.SNIPER_STATUS_NAME)).hasText(equalTo(auctionStatus.toString()));
+		new JTableDriver(this).hasCell(withLabelText(equalTo(auctionStatus.toString())));
 	}
 }
