95196cc99d8ee3c1ca5a2ab4af568d69de8b581d
yujiorama <yujiorama+github@gmail.com>
2012-05-09 17:27:51 +0900

    Shortening the Event Path
    transmit state chang event from Auction Server to Swing UI,
    there were wast path.

--- goos-auction/src/main/java/com/github/yujiorama/auctionsinper/MainWindow.java
+++ goos-auction/src/main/java/com/github/yujiorama/auctionsinper/MainWindow.java
@@ -16,28 +16,33 @@ public class MainWindow extends JFrame {
 	public static final String MAIN_WINDOW_NAME = "auction-sniper";
 	public static final String SNIPER_STATUS_NAME = "sniper status";
 	public static final String SNIPER_TABLE_NAME = "sniper table";
-	private SniperTableModel sniperTableModel = new SniperTableModel();
-	private JTable snipersTable = newTable(SNIPER_TABLE_NAME);
+	private final SniperTableModel snipers;
+	private JTable snipersTable;
 
-	public MainWindow() {
+	public MainWindow(final SniperTableModel snipers) {
 		super(APPLICATION_NAME);
+		this.snipers = snipers;
+		
 		setName(MAIN_WINDOW_NAME);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(new Rectangle(new Point(50, 50), new Dimension(320, 240)));
 		
+		this.snipersTable = newTable(SNIPER_TABLE_NAME);
+
 		Container container = getContentPane();
 		container.setLayout(new BorderLayout());
 		container.add(new JScrollPane(snipersTable), BorderLayout.CENTER);
+		
 		setVisible(true);
 	}
 
 	private JTable newTable(String name) {
-		JTable table = new JTable(sniperTableModel);
+		JTable table = new JTable(snipers);
 		table.setName(name);
 		return table;
 	}
 
 	public void sniperStatusChanged(SniperSnapshot newSnapshot) {
-		sniperTableModel.sniperStatusChanged(newSnapshot);
+		snipers.sniperStatusChanged(newSnapshot);
 	}
 }
